722c3ecdf7b82a973fd4d49373f13cc5d77f6ec5
Schwappy <paul.schwappach@gmail.com>
2016-04-19 15:02:33 -0700

    Update Card.h
    comment clean-up

--- Card.h
+++ Card.h
@@ -1,16 +1,16 @@
 //* * * * * * * * * * * * * * * * * * * * * * * //
-//  Author:		Paul Schwappach					//
-//	Professor:	George Sfakianakis				//
-//	Class:		CSIS 297 - Intermediate C++		//
-//	Assignment:	Semester Project				//
-//	Date:		02/08/2016						//
+// Author:	Paul Schwappach			//
+// Professor:	George Sfakianakis		//
+// Class:	CSIS 297 - Intermediate C++	//
+// Assignment:	Semester Project		//
+// Date:	02/08/2016			//
 //* * * * * * * * * * * * * * * * * * * * * * * //
 
 //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *	//
-//  About Card class (header file)								//
-//		The card class allows the creation of a single card		//
-//	that can be then used by it's subclasses.  A Deck class,	//
-//	for example, is a collection of cards that can be shuffled	//
+//  About Card class (header file)				//
+//	The card class allows the creation of a single card	//
+// that can be then used by it's subclasses.  A Deck class,	//
+// for example, is a collection of cards that can be shuffled	//
 //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *	//
 
 #ifndef CARD_H
@@ -22,13 +22,13 @@
 class Card
 {
 private:
-	char suit;			// card suit
-	char rank;			// card rank
+	char suit;	// card suit
+	char rank;	// card rank
 
 public:
 	Card();
 	Card(char, char);	// Constructor
-	//~Card();			// Destructor
+	//~Card();		// Destructor
 	void setSuit(char);	// Mutator
 	void setRank(char);	// Mutator
 	char getSuit() const;	// Accessor
